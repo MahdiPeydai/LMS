f95575b4798ecad4f3505cee7c59331b0ddfab32
MahdiPeydai <mahdipeydai@gmail.com>
2023-08-02 20:22:17 +0330

    CartCheck middleware modified (v.1)

--- checkout/middleware.py
+++ checkout/middleware.py
@@ -1,8 +1,6 @@
-from django.conf import settings
+from django.core.cache import cache
 
 from checkout.models import Cart
-
-import jwt
 
 
 class CartCheck:
@@ -10,31 +8,18 @@ class CartCheck:
         self.get_response = get_response
 
     def __call__(self, request):
-        if 'cart' in request.COOKIES:
-            token = request.COOKIES.get('cart')
-            token = jwt.decode(token, settings.SECRET_KEY, algorithms=['HS256'])
-            cart_id = token['cart']
-            print(cart_id)
-            cart = Cart.objects.get(id=cart_id)
-
-            if (not cart.user) and request.user:
-                cart.user = request.user.id
-                cart.save()
+        if cache.has_key(f'cart_session_{request.session.session_key}'):
+            cart = cache.get(f'cart_session_{request.session.session_key}')
         else:
             if request.user.is_authenticated:
-                user = request.user
-                if Cart.objects.filter(user=user.id).exists():
-                    cart = Cart.objects.filter(user=user).first()
+                if Cart.objects.filter(user=request.user).exists():
+                    cart = Cart.objects.filter(user=request.user).first()
                 else:
-                    cart = Cart(user=user)
+                    cart = Cart(user=request.user)
                     cart.save()
             else:
                 cart = Cart()
                 cart.save()
-
-        request.cart = cart
-        token = jwt.encode({'cart': cart.id}, settings.SECRET_KEY, algorithm='HS256')
+        cache.set(f'cart_session_{request.session.session_key}', cart)
         response = self.get_response(request)
-        response.set_cookie('cart', token, max_age=86400)
-
         return response
