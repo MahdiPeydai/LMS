6d3f3314509d76009d3bdbf5d53b3e2718a363c1
MahdiPeydai <mahdipeydai@gmail.com>
2023-08-03 17:51:18 +0330

    CartChech middleware removed and AnonymousCart middleware added to MIDDLEWARE in setting.py

--- OnlineLearning/settings.py
+++ OnlineLearning/settings.py
@@ -64,7 +64,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'checkout.middleware.CartCheck'
+    'checkout.middleware.AnonymousCart'
 ]
 
 ROOT_URLCONF = 'OnlineLearning.urls'
