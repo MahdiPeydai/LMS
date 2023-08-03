3895d566d798756da8365975dc3e2bb34b799f95
MahdiPeydai <mahdipeydai@gmail.com>
2023-08-03 17:49:51 +0330

    CartChech middleware removed and AnonymousCart middleware added to create cart for anonymous user and set it to cookie

--- checkout/middleware.py
+++ checkout/middleware.py
@@ -1,25 +1,14 @@
-from django.core.cache import cache
-
 from checkout.models import Cart
 
 
-class CartCheck:
+class AnonymousCart:
     def __init__(self, get_response):
         self.get_response = get_response
 
     def __call__(self, request):
-        if cache.has_key(f'cart_session_{request.session.session_key}'):
-            cart = cache.get(f'cart_session_{request.session.session_key}')
-        else:
-            if request.user.is_authenticated:
-                if Cart.objects.filter(user=request.user).exists():
-                    cart = Cart.objects.filter(user=request.user).first()
-                else:
-                    cart = Cart(user=request.user)
-                    cart.save()
-            else:
-                cart = Cart()
-                cart.save()
-        cache.set(f'cart_session_{request.session.session_key}', cart)
         response = self.get_response(request)
+        if (not request.user.is_authenticated) and ('cart' not in request.COOKIES):
+            cart = Cart()
+            cart.save()
+            response.set_cookie('cart', cart.id)
         return response
