6d490dd399acc1b23e75aa0ac6801e507ec50538
MahdiPeydai <mahdipeydai@gmail.com>
2023-08-04 19:57:23 +0330

    middleware name changed to guestSession and it create session for guests

--- OnlineLearning/settings.py
+++ OnlineLearning/settings.py
@@ -64,7 +64,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'checkout.middleware.AnonymousCart'
+    'checkout.middleware.GuestSession'
 ]
 
 ROOT_URLCONF = 'OnlineLearning.urls'
--- checkout/middleware.py
+++ checkout/middleware.py
@@ -1,14 +1,12 @@
 from checkout.models import Cart
 
 
-class AnonymousCart:
+class GuestSession:
     def __init__(self, get_response):
         self.get_response = get_response
 
     def __call__(self, request):
         response = self.get_response(request)
-        if (not request.user.is_authenticated) and ('cart' not in request.COOKIES):
-            cart = Cart()
-            cart.save()
-            response.set_cookie('cart', cart.id)
+        if not request.user.is_authenticated:
+            request.session.create()
         return response
