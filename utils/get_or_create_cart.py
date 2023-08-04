3fa3a3a082de335676109a086c67937285088234
MahdiPeydai <mahdipeydai@gmail.com>
2023-08-04 18:58:20 +0330

    handle creating cart for user/guest

--- utils/get_or_create_cart.py
+++ utils/get_or_create_cart.py
@@ -1,16 +1,19 @@
 from django.core.cache import cache
 
 from checkout.models import Cart
+from accounts.models import Guest
 
 
 def get_cart(request):
-    user = request.user
-    if user.is_authenticated:
-        user_cart = Cart.objects.filter(user=user).first()
-        if not user_cart:
-            user_cart = Cart.objects.create(user_type='authenticated', user_id=user.id)
-        cache.set(f'cart_session_{request.session.session_key}', user_cart)
-    else:
-
     cart = cache.get(f'cart_session_{request.session.session_key}')
+    if not cart:
+        user = request.user
+        if user.is_authenticated:
+            cart = Cart.objects.filter(user=user).first()
+            if not cart:
+                cart = Cart.objects.create(user=user)
+        else:
+            guest = Guest.objects.create(session_id=request.session.session_key)
+            cart = Cart.objects.create(user=guest)
+        cache.set(f'cart_session_{request.session.session_key}', cart)
     return cart
